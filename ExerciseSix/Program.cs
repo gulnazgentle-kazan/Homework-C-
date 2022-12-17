1bfca44d1a02f8f56ced4c601d2e60f43d613015
gulnazgentle-kazan <gulnazvp@mail.ru>
2022-12-18 01:36:54 +0300

    did and added Exercise 6 Homework 1

---
+++ ExerciseSix/Program.cs
@@ -0,0 +1,20 @@
+﻿/*Задача 6: Напишите программу, 
+которая на вход принимает число и 
+выдаёт, является ли число чётным 
+(делится ли оно на два без остатка).
+
+4 -> да
+-3 -> нет
+7 -> нет*/
+
+Console.WriteLine("Введите любое целое число");
+int number = Convert.ToInt32(Console.ReadLine());
+bool evenNumber = Convert.ToBoolean(number % 2);
+if (evenNumber)
+{
+    Console.WriteLine($"Число {number} является нечетным");
+}
+else
+{
+    Console.WriteLine($"Число {number} является четным");
+}
