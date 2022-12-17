a0326089582581324e676d3e2bc2e66b5002caf5
gulnazgentle-kazan <gulnazvp@mail.ru>
2022-12-18 02:18:06 +0300

    did and added Exaercise №8 Homework 1

---
+++ ExerciseEight/Program.cs
@@ -0,0 +1,28 @@
+﻿/*Задача 8: Напишите программу, 
+которая на вход принимает число (N), 
+а на выходе показывает все чётные числа от 1 до N.
+
+5 -> 2, 4
+8 -> 2, 4, 6, 8*/
+
+/*Console.WriteLine("Введите любое целое число");
+int number = Convert.ToInt32(Console.ReadLine());
+bool evenNumber = Convert.ToBoolean(number % 2);
+int count = 1;
+while (count <= number)
+{
+    if (evenNumber)
+    {
+        Console.Write($"{count + 1}");
+        count++;
+    }
+}*/
+
+Console.WriteLine("Введите любое целое число");
+int number = int.Parse(Console.ReadLine());
+int count = 2;
+while (count <= number)
+{
+    Console.WriteLine(count);
+    count += 2;
+}
